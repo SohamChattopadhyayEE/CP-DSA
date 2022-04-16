22476617fa84b743a4b60a9454ba8fbbbd1b3916
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-04-16 11:05:10 +0530

    Create 746. Min Cost Climbing Stairs.java

---
+++ Leetcode/DP/746. Min Cost Climbing Stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int[] dp = new int[n+1];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        for(int i=2; i<n+1; i++){
+            if(i!=n)
+                dp[i] = cost[i]+Math.min(dp[i-1], dp[i-2]);
+            else
+                dp[i] = Math.min(dp[i-1], dp[i-2]);
+        }
+        return dp[n];
+    }
+}
