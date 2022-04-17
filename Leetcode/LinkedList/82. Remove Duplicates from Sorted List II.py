45b7ad922db000c261426659efb72f310e28bd65
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-04-17 21:04:45 +0530

    Update 82. Remove Duplicates from Sorted List II.py

--- Leetcode/LinkedList/82. Remove Duplicates from Sorted List II.py
+++ Leetcode/LinkedList/82. Remove Duplicates from Sorted List II.py
@@ -1,3 +1,16 @@
+'''
+Algorithm:
+------------------------------------------------------------------------------------
+1. Traverse through the list and make a frequecy array using a dictionary (Map).
+2. Have two pointer nodes `curr` and `prev`, where the `curr` is pointing to the 
+head and `prev.next` points to the `curr`.
+3. Initiate a dummy node say `node` such that
+                    node.next=prev
+4. Re-iterate through the list again and skip those nodes havin occuring frequency
+more than 1.
+5. Return node.next.next
+'''
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
