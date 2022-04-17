31528de85aed882dbfb1875f8a05adb4346dc9a4
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-04-17 10:49:54 +0530

    Update 121. Best Time to Buy and Sell Stock.py

--- Leetcode/DP/121. Best Time to Buy and Sell Stock.py
+++ Leetcode/DP/121. Best Time to Buy and Sell Stock.py
@@ -2,8 +2,8 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
         
-        /* Prepare an array of length same as that of prices, ith element of which is 
-        the maximum element positioned at the right of ith element of the array prices*/
+        '''Prepare an array of length same as that of prices, ith element of which is 
+        the maximum element positioned at the right of ith element of the array prices'''
         rightMax = [0]*n
         m=prices[n-1]
         for i in range(n-2, -1, -1):
@@ -13,8 +13,8 @@ class Solution:
             else:
                 rightMax[i]=m
         
-        /* Find the maximum difference between ith elements of the rightMax array and the
-        prices array*/
+        '''Find the maximum difference between ith elements of the rightMax array and the
+        prices array'''
         m=0
         for i in range(n):
             m=max(m, rightMax[i]-prices[i])
