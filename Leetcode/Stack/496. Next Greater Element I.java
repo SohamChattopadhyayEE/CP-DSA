3665d34b619d22c9dcfa99bf5db3a22bd74438f7
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-04-17 11:32:46 +0530

    Update 496. Next Greater Element I.java

--- Leetcode/Stack/496. Next Greater Element I.java
+++ Leetcode/Stack/496. Next Greater Element I.java
@@ -1,3 +1,18 @@
+/* Algorithm:
+1. The idea is to prepare a HashMap having the 'keys' being the elements of nums2
+and corresponding 'values' being the next greater elements with the help of a stack.
+2. Traverse from the end of the nums2.
+3. For any element of nums2, if the stack is found empty put the next greater element 
+of it as -1 in the Map
+4. If the peek element of the stack is less than the ith element of the array nums2,
+pop it up. Keep doing this as long as greater element occurs or stack becomes empty.
+5. Thereafter repear step 3.
+6. Hence, we have a HashMap containing elements of nums2 and next greater elements of 
+each element.
+7. Finally, return an array of length same as that of nums1 where ith element of which
+is the next grater element in nums2 of ith element of nums1 extracted from the HashMap
+filled earlier.*/
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
