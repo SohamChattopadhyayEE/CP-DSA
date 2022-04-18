7aa429c5d555b47b52872fc21c2e692632e5de61
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-04-18 09:39:27 +0530

    Create 230. Kth Smallest Element in a BST.java

---
+++ Leetcode/Binary search tree/230. Kth Smallest Element in a BST.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+/*
+Solution.1:
+
+1. Perform `Inorder` traversal and fill inorder list which happens to 
+be a sorted list.
+2. Since it is 0th indexed list, return the (k-1)th element from the 
+list which is the kth smallest element.
+*/
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res.get(k-1);
+    }
+    public void inorder(TreeNode root, List<Integer> res){
+        if(root != null){
+            inorder(root.left, res);
+            res.add(root.val);
+            inorder(root.right, res);
+        }
+    }
+}
