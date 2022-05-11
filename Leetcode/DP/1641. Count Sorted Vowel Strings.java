5b52bc68bcc41ffa2d18f5a8ffd02ab900cad417
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-11 11:54:14 +0530

    Update 1641. Count Sorted Vowel Strings.java

--- Leetcode/DP/1641. Count Sorted Vowel Strings.java
+++ Leetcode/DP/1641. Count Sorted Vowel Strings.java
@@ -1,15 +1,31 @@
 /*
 Algorithm: 
-    vowels / n ->  0   1   2   3   4   5
+-----------------------------------------------------------------------
+1. Have a dp table with 7 rows and `n` number of columns where
+   0th row is for `null`, 1-5 are for each of the vowel from
+   `a` to `e` and the 6th holds the total number, i.e the sum
+   of row 1 to row 5.
+2. The intuation is-
+        for ith column the number of words being started with
+        say jth vowel is the summation of all words of (i-1)th
+        column except those who start with vowels which come
+        lexicographically before to that of the ith vowel. 
+        Therefore, the dynamic relationship we have here is -
+   dp[i][j] = dp[i-1][6]-sum(dp[i-1][j-1]+dp[i-1][j-1]+...dp[i-1][0])
+   
+
+    /----------\
+    |vowels / n| ->  0   1   2   3   4   5
+    \----------/
     |
     V
-    null           0   0   0   0   0   0   
-    a              0   1   5   15  35  70
-    e              0   1   4   10  20  35
-    i              0   1   3   6   10  15
-    o              0   1   2   3   4   5
-    u              0   1   1   1   1   1
-    total          0   5   15  35  70  126
+    null             0   0   0   0   0   0   
+    a                0   1   5   15  35  70
+    e                0   1   4   10  20  35
+    i                0   1   3   6   10  15
+    o                0   1   2   3   4   5
+    u                0   1   1   1   1   1
+    total            0   5   15  35  70  126
 
 */
 
