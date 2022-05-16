72726bef3d3ec1086a0b7d4025d5e5e36a87f106
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-16 11:33:49 +0530

    Update 1091. Shortest Path in Binary Matrix.java

--- Leetcode/BFS/1091. Shortest Path in Binary Matrix.java
+++ Leetcode/BFS/1091. Shortest Path in Binary Matrix.java
@@ -1,7 +1,20 @@
 /*
 Algorithm:
--------------------------------------------------------------------------------
--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+1. Perform BFS in the input matrix from the starting point (0,0). 
+2. Maintain a HashMap to save the information of how far is a point from the 
+   source point. 
+3. Once a point is visited, change the value of that point from `0` to `1` or
+   mark the point as visited. 
+4. During the BFS while visiting a valid point in the grid, put that point in
+   the map as key and corresponding value is the distance from the source 
+   (0,0) which is 1 more than that of the immidiate parent point of the current
+   point. 
+5. When the BFS call is over, return the value stored in correspondance to the
+   final point (n-1, n-1) in the map. 
+6. If the map does not contain that point, that is if the point is not visited
+   return -1.
+--------------------------------------------------------------------------------
 */
 
 class Solution {
