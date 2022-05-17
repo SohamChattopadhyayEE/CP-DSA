36dad2536fe47f5cd40694b3c2ccf5d389fa44dd
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-17 10:48:43 +0530

    Update 1079. Letter Tile Possibilities.java

--- Leetcode/Backtrcking/1079. Letter Tile Possibilities.java
+++ Leetcode/Backtrcking/1079. Letter Tile Possibilities.java
@@ -1,3 +1,19 @@
+/*
+Algorithm:
+----------------------------------------------------------------------------------------
+1. Simple `backtracking` approach. 
+2. The idea is to add characters one by one of the input string (tiles) to a temporary
+   string which is further added to a HashSet to avoid repeatation. 
+3. While doing so the characters which are once visited should be skipped for that 
+   maintain a `visited` array of size same as that of the entire string.
+4. In each recursion call, add the temporary strings into the HashSet.
+5. When the recursion call is over, the set is filled with all strings, which includes
+   the `null` as well.
+6. Hence return the size of the set -1. That `-1` corresponds to the `null` string. 
+----------------------------------------------------------------------------------------
+*/
+
+
 class Solution {
     public int numTilePossibilities(String tiles) {
         Set<String> set = new HashSet<>();
