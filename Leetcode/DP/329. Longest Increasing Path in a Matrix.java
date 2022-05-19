f9b6c0aa47feb8b9852933f025502dab24a083c5
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-19 11:07:19 +0530

    Update 329. Longest Increasing Path in a Matrix.java

--- Leetcode/DP/329. Longest Increasing Path in a Matrix.java
+++ Leetcode/DP/329. Longest Increasing Path in a Matrix.java
@@ -1,3 +1,14 @@
+/*
+Algorithm: DFS
+---------------------------------------------------------------------------------------------
+1. Traverse through the entire matrix.
+2. Perform DFS form each of the points of the matrix to find the longest increasing path
+   staring from that point.
+3. Return the path with maximum length among all the longest paths generated from each point
+   of the matrix.
+---------------------------------------------------------------------------------------------
+*/
+
 class Solution {
     int m, n;
     int[][] mat;
@@ -26,6 +37,49 @@ class Solution {
             }
         }
         return res+1;
+        
+    }
+    public boolean isValid(int x, int y){
+        return x>=0 && x<m && y>=0 && y<n;
+    }
+}
+
+/*
+Algorithm: DP
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+*/
+class Solution {
+    int m, n;
+    int[][] mat;
+    int[][] dp;
+    int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    public int longestIncreasingPath(int[][] matrix) {
+        mat = matrix;
+        m = matrix.length;
+        n = matrix[0].length;
+        dp = new int[m][n];
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                max = Math.max(max, dfs(i, j, 1));
+            }
+        }
+        return max;
+    }
+    public int dfs(int x, int y, int len){
+        if(dp[x][y] != 0)
+            return dp[x][y];
+        int res = 0;
+        for(int[] dir : dirs){
+            int xn = x+dir[0];
+            int yn = y+dir[1];
+            if(isValid(xn, yn) && mat[xn][yn]>mat[x][y] ){
+                int temp = dfs(xn, yn, len+1);
+                res = Math.max(res, temp);
+            }
+        }
+        return dp[x][y] = res+1;
         
     }
     public boolean isValid(int x, int y){
