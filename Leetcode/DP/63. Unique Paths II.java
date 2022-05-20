8a05b1b2b03f751de6525e6691c23fb9d7ecdb8b
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-20 10:05:36 +0530

    Update 63. Unique Paths II.java

--- Leetcode/DP/63. Unique Paths II.java
+++ Leetcode/DP/63. Unique Paths II.java
@@ -1,5 +1,5 @@
 /*
-Solution 1: Memoization
+Solution 1: Memoization (Top-Down approach) - 1 ms, 43 mb
 Algorithm:
 --------------------------------------------------------------------------------
 1. Perform DFS from the point (0,0) to find paths to the bottom right corner
@@ -45,5 +45,40 @@ class Solution {
     }
     public boolean isValid(int x, int y){
         return x>=0 && x<m && y>=0 && y<n;
+    }
+}
+
+/*
+Solution 1: Tabulation (Bottom-Up approach) - 0 ms, 30 mb
+Algorithm:
+--------------------------------------------------------------------------------
+1. Have a DP table with the base condition as if the starting point is an 
+   obstacle then (0,0) of the DP table be 0.
+2. Iterate through the matrix and if at any point say (x, y), an obstacle is
+   found, make value of the DP table at that point be 0.
+3. 
+--------------------------------------------------------------------------------
+*/
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) 
+            return 0;
+        dp[0][0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else if(i==0){
+                    if(j>0) dp[i][j] = dp[i][j-1];
+                }
+                else if(j==0){
+                    if(i>0) dp[i][j] = dp[i-1][j];
+                }
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
