09566c4b0895d7901dc029aededbe48ce4524775
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-20 11:08:18 +0530

    Update 1286. Iterator for Combination.java

--- Leetcode/Design Data Structures/1286. Iterator for Combination.java
+++ Leetcode/Design Data Structures/1286. Iterator for Combination.java
@@ -1,10 +1,19 @@
 /*
 Algorithm:
--------------------------------------------------------------------------------
+---------------------------------------------------------------------------------
 1. Build a Priority Queue containing all the combinations of given size of 
-   strings containing the characters from the input string such that the are 
-   automatically is sorted format.
-2. 
+   strings containing the characters from the input string such that they are 
+   automatically in lexicographically sorted form.
+2. perform `BackTracking` algorithm to fill the Priority Queue with combinations
+   of given length. 
+3. The base condition of the recursion call is when the length of the temporary
+   string is equal to the specified length, add that string to the Priority 
+   Queue and backtrack.
+4. When the method `next()` is called, return the String which is at the top of
+   the Priority Queue.
+5. In case of the method `hasNext()`, return `true` if the the Priority Queue
+   is not null. Otherwise, return `false`.
+---------------------------------------------------------------------------------
 */
 
 class CombinationIterator {
