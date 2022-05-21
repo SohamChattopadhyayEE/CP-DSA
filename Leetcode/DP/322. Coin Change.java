22ae3a121cd1114bf4436ebce5dcfa01f9536737
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-21 10:21:25 +0530

    Update 322. Coin Change.java

--- Leetcode/DP/322. Coin Change.java
+++ Leetcode/DP/322. Coin Change.java
@@ -59,3 +59,25 @@ class Solution {
         return dp[n] = res;
     }
 }
+
+/*
+Solution 3: Dynamic Programming (Tabulation) - Accepted (20 ms, 32 mb)
+Algorithm:
+----------------------------------------------------------------------------
+----------------------------------------------------------------------------
+*/
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i=1; i<=amount; i++){
+            for(int j=0; j<coins.length; j++){
+                if(i-coins[j] >=0 && dp[i-coins[j]] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i], dp[i-coins[j]]+1);
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
