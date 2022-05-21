e3cdda959f681b60debb4b8a7875ae6e5cd6d548
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-21 12:45:40 +0530

    Update 78. Subsets.java

--- Leetcode/Backtrcking/78. Subsets.java
+++ Leetcode/Backtrcking/78. Subsets.java
@@ -1,3 +1,20 @@
+/*
+Algorithm:
+----------------------------------------------------------------------------------------------------------
+1. Naive `Backtracking` approach is taken for finding all the combinations.
+2. Maintain an indexing pointer to indicate a particular element of the array during the call.
+3. The base condition of the Backtracking method is, if the indexing pointer points to the last element of
+   the input array, add the list of elements to the resultant list of combination.
+4. When the indexing pointer is less than that of the lebgth of the input array, addressing two different
+   cases is needed. One of them is not consindering current element for the current subset and moving 
+   forward with unitely incrementing the index pointer. The other case is simply considerding the element 
+   by adding it to the list responsible for representing the current combination. 
+5. Once the recursive backtracking call is over, return the list of all combinations of the input array. 
+----------------------------------------------------------------------------------------------------------
+
+*/
+
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
