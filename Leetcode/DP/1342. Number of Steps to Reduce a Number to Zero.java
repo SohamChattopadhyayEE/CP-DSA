144996ec0b6d69724df6bb85fc126d7524b26e03
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-27 10:44:39 +0530

    Update 1342. Number of Steps to Reduce a Number to Zero.java

--- Leetcode/DP/1342. Number of Steps to Reduce a Number to Zero.java
+++ Leetcode/DP/1342. Number of Steps to Reduce a Number to Zero.java
@@ -4,7 +4,9 @@ Algorithm:
 1. Initiate a DP table of length equal to the input `num`.
 2. Set the base condition of 0th element and 1st element of the DP table is 
    0 and 1 respectively.
-3. 
+3. Iterate from 2 to num with a iterator say `i` in such a way that if i is 
+   divisible by 2 then `dp[i] = 1+dp[i/2]`. Otherwise, `dp[i] = 1+dp[i-1]`.
+4. Return the last element of the DP table dp[num].
 -----------------------------------------------------------------------------
 */
 
