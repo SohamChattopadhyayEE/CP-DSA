d8e4ecb34748286eba24adf6443954ff167d761e
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-05-28 09:49:59 +0530

    Update 268. Missing Number.java

--- Leetcode/Others/268. Missing Number.java
+++ Leetcode/Others/268. Missing Number.java
@@ -1,7 +1,6 @@
 /*
-Solution 1: Time : O(nlogn), Space : O(1)
+Solution 1: Sorting || Time : O(nlogn), Space : O(1)
 */
-
 class Solution {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
@@ -9,5 +8,17 @@ class Solution {
             if(i!=nums[i])
                 return i;
         return nums.length;
+    }
+}
+
+/*
+Solution 2: Bit manipulation || Time: O(n), Space: O(1)
+*/
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = nums.length;
+        for(int i=0; i<nums.length; i++)
+            res ^= i^nums[i];
+        return res;
     }
 }
