775811b80446bce0579dfe25a65d4ff37c06a9da
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-06-06 09:28:12 +0530

    Update 160. Intersection of Two Linked Lists.java

--- Leetcode/LinkedList/160. Intersection of Two Linked Lists.java
+++ Leetcode/LinkedList/160. Intersection of Two Linked Lists.java
@@ -1,3 +1,15 @@
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode l1 = headA;
