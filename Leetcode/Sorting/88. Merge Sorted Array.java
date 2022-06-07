a923e6bb5fc3b59cba88b9d6c71af4851c7df488
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-06-07 09:59:08 +0530

    Update 88. Merge Sorted Array.java

--- Leetcode/Sorting/88. Merge Sorted Array.java
+++ Leetcode/Sorting/88. Merge Sorted Array.java
@@ -1,6 +1,14 @@
 /*
 Algorithm:
 ------------------------------------------------------------------------------
+1. Initiate two pointers (`i` and `j`) pointing to the `0th` element of the arrays `nums1` and `nums2` respectively. In addition to this,
+2. Iterate through both of the arrays in a single while loop as long as none of the pointers reaches the length of
+   any one of the array. 
+3. Each time compare the elements of the arrays and the smaller one is to be put to the dynamic `list` initiated before
+   than that.
+4. Once the while loop is over, the smaller array is completely interated over and the larger one is still left. 
+5. Iterate to the rest of the elements of the larger array and keep adding them to the list.
+6. Replace the elements of `nums1` with the elements of the list. 
 ------------------------------------------------------------------------------
 */
 
