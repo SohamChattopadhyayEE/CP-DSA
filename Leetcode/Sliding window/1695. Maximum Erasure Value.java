6ce391cce9096caae4a1ebfc746fe650b3e59da4
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-06-12 10:39:46 +0530

    Update 1695. Maximum Erasure Value.java

--- Leetcode/Sliding window/1695. Maximum Erasure Value.java
+++ Leetcode/Sliding window/1695. Maximum Erasure Value.java
@@ -1,3 +1,18 @@
+/*
+Algorithm:
+------------------------------------------------------------------------------------------------------------------------
+1. Typical `sliding window` based approach, where two pointers `left` and `right` denotes two extremeties of a subarray
+   which satisfies the conditions given and maximum sum of such subarrays are to be found. 
+2. To do so, a `HashSet` is maintained which keeps track of the fact that no repeated element is present in the subarray
+3. While iterating over the input array, elements are being added to the `set` and to an integer variable holding the 
+   `current sum` of the subarray till no repeatation of elements takes place.
+4. While repeatation occurs, removing from the `set` as well as substraction from the `current sum` from the `left` is 
+   performed.
+5. Among all such subarrays which satisfy the given condition of no repeatitive elements, the maximum sum is measured
+   and returned after the loop is over. 
+------------------------------------------------------------------------------------------------------------------------
+*/
+
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
         int n=nums.length;
