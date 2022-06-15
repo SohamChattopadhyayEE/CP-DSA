3363c7aee6683405fb1d2d33447623c1d3350360
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-06-15 10:31:40 +0530

    Update 1048. Longest String Chain.java

--- Leetcode/DP/1048. Longest String Chain.java
+++ Leetcode/DP/1048. Longest String Chain.java
@@ -1,16 +1,19 @@
 class Solution {
     public int longestStrChain(String[] words) {
         int n = words.length;
+        if(n==1)
+            return 1;
         Arrays.sort(words, (a, b) -> a.length()-b.length());
         int[] lis = new int[n];
         Arrays.fill(lis, 1);
-        for(int i=1; i<n; i++)
-            for(int j=0; j<i; j++)
+        int max = 0;
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
                 if(isPred(words[j], words[i]))
                     lis[i] = Math.max(lis[i], lis[j]+1);
-        int max = 0;
-        for(int m : lis)
-            max = Math.max(max, m);
+            }
+            max = Math.max(max, lis[i]);
+        }
         return max;
     }
     public boolean isPred(String s1, String s2){
