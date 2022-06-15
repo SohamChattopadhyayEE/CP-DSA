4b9ae2614429c05c274c71d4dd811c7d94e6c1ff
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2022-06-15 11:02:16 +0530

    Update 1048. Longest String Chain.java

--- Leetcode/DP/1048. Longest String Chain.java
+++ Leetcode/DP/1048. Longest String Chain.java
@@ -1,3 +1,16 @@
+/*
+Algorithm:
+----------------------------------------------------------------------------------------------------------------
+1. The given problem is simply a variation of typical `Longest Increasing Subsequence (LIS)` problem.  
+2. Similar to `LIS` algorithm, here also we need to traverse through the entire array and maintain a dynamic
+   array of length similar to the input array, `ith` element of which denotes the length of longest subsequence
+   ending at `ith` element. 
+3. Since `rearrangment` is allowed here, the words firstly need to get sorted based on the lengths of the words.
+4. Thereafter the algorithm followed in `LIS` is applied here where the comparing is done by checking if one 
+   string is predecessor to the other or not. 
+5. The maximum value of the `LIS` array is returned.  
+----------------------------------------------------------------------------------------------------------------
+*/
 class Solution {
     public int longestStrChain(String[] words) {
         int n = words.length;
