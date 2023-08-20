832d74ffa1fcafc04a0af381aae22b982e17e07e
Soham Chattopadhyay <67259890+SohamChattopadhyayEE@users.noreply.github.com>
2023-08-20 12:26:36 +0530

    Create topologicalSorting.java

---
+++ Algorithms/topologicalSorting.java
@@ -0,0 +1,25 @@
+class Solution{
+    public List<Integer> topologicalSort(Map<Integer, List<Integer>> graph, int[] indegree) {
+        List<Integer> visited = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        for (Integer key : graph.keySet()) {
+            if (indegree[key] == 0) {
+                stack.add(key);
+            }
+        }
+        
+        while (!stack.isEmpty()) {
+            Integer curr = stack.pop();
+            visited.add(curr);
+            
+            for (Integer prev : graph.get(curr)) {
+                indegree[prev]--;
+                if (indegree[prev] == 0) {
+                    stack.add(prev);
+                }
+            }
+        }
+
+        return visited.size() == graph.size() ? visited : new ArrayList<>();
+    }
+}
